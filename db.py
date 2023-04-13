4791d039567cf9e0cb5a00de79a6ca4de5db5549
Pedro Falconi <82791269+PedroShimpa@users.noreply.github.com>
2023-04-13 15:00:37 -0300

    correções na lista

--- db.py
+++ db.py
@@ -1,4 +1,7 @@
 import sqlite3
+from datetime import datetime
+
+
 db_name = 'database'
 
 conn = sqlite3.connect(db_name+".db")
@@ -10,24 +13,30 @@ def criar_base():
     create table if not exists tarefas (
         id integer primary key autoincrement,
         tarefa text,
-        prioridade text,
-        concluido integer
+        concluido integer,
+        created text,
+        concluida_em text
     )
     """)
 
+
 def add_tarefa(tarefa):
+
+    created = datetime.now()
     conn.execute(
-        "insert into tarefas (tarefa, concluido) values (?, 0)", (tarefa, ))
+        "insert into tarefas (tarefa, concluido,created) values (?,?,?)", (tarefa, 0, created, ))
     conn.commit()
 
-   
 
 def remover_tarefa(id):
     conn.execute("delete from tarefas where id = ?", (id))
     conn.commit()
 
+
 def concluir_tarefa(id):
-    conn.execute("update tarefas set concluido = ?", (1, ))
+    concluida_em = datetime.now()
+    conn.execute(
+        "update tarefas set concluido = ?, concluida_em = ?", (1, concluida_em, ))
     conn.commit()
 
 
